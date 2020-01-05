5e34464e407bee59cd6a64d192ad24b657bd4a39
MarcinMichna <marcinxmichna@gmail.com>
2020-01-05 12:31:29 +0100

    command

---
+++ src/command/Main.java
@@ -0,0 +1,16 @@
+package command;
+
+public class Main
+{
+
+
+    public static void main(String[] args)
+    {
+        Stock stock = new Stock();
+
+        Broker broker = new Broker();
+        broker.takeOrder(new BuyStock(stock));
+        broker.takeOrder(new SellStock(stock));
+        broker.placeOrders();
+    }
+}
---
+++ src/command/Stock.java
@@ -0,0 +1,17 @@
+package command;
+
+public class Stock
+{
+    private String name = "CDP RED";
+    private int quantity = 10;
+
+    public void buy()
+    {
+        System.out.println("buying stock");
+    }
+
+    public void sell()
+    {
+        System.out.println("selling stock");
+    }
+}
